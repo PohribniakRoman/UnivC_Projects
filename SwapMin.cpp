2c939dbd81a26dc0d087d490871f548edaea374c
roman.pohribniak <roman.pohribniak@gmail.com>
2023-01-29 15:47:00 +0200

    SwapMin v1.1

--- SwapMin.cpp
+++ SwapMin.cpp
@@ -2,28 +2,24 @@
 
 using namespace std;
 
-
-template<size_t r>
-void swap_min(int (*m)[r], unsigned rows, unsigned cols){
-    int min = m[0][0];
+void swap_min(int *arr[], unsigned rows, unsigned cols){
+    int min = arr[0][0];
     int lowestNumRow = 0;
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            if(min > m[i][j]){
-                min = m[i][j];
+            if(min > arr[i][j]){
+                min = arr[i][j];
                 lowestNumRow = i;
             }
         }
     }
-    swap(m[0],m[lowestNumRow]);
+    swap(arr[0],arr[lowestNumRow]);
 }
 
-
-template<size_t a>
-void visualise(int (*m)[a], unsigned rows, unsigned cols){
+void visualize(int *arr[], unsigned rows, unsigned cols){
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            cout << m[i][j] << "\t";
+            cout << arr[i][j] << "\t";
         }
         cout << "|" << endl;
     }
@@ -33,14 +29,40 @@ void visualise(int (*m)[a], unsigned rows, unsigned cols){
 int main(){
     const int rows = 5;
     const int cols = 4;
-    int arr[rows][cols] = {
-            {10,  22,   13,  15},
-            {120, 130,  150, 160,},
-            {1,   2,    3,   5},
-            {14,  3000, -12, 16},
-            {66,  73,84,92}
-    };
-    swap_min( arr,rows, cols);
-    visualise(arr,rows, cols);
+    int** arr = new int*[rows];
+    cout << "Enter data:\n";
+    for (int i = 0; i < 5; ++i) {
+        arr[i] = new int[cols];
+        for (int j = 0; j < 4; ++j) {
+            cin >> arr[i][j];
+        }
+    }
+    cout << "Before refactor: \n";
+    visualize(arr, cols, cols);
+    swap_min(arr, rows, cols);
+    cout << "After refactor: \n";
+    visualize(arr, cols, cols);
     return 0;
 }
+
+//input data
+//10
+//22
+//13
+//15
+//120
+//130
+//150
+//160
+//1
+//2
+//3
+//5
+//14
+//3000
+//-12
+//16
+//66
+//73
+//84
+//92
